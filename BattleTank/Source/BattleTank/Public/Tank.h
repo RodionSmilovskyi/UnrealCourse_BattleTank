9cb7e0fc95c83c067b04dd3e78c4fdb37fd7f5dc
Rodion Smilovskyi <smelovsky86@gmail.com>
2017-07-08 22:17:57 -0400

    Lecture 4-134.

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -29,4 +29,7 @@ public:
 
 	UFUNCTION(BlueprintCallable, Category = Setup)
 	void SetBarrelReference(UStaticMeshComponent* BarrelToSet);
+
+	UPROPERTY(EditAnywhere, Category = Firing)
+	float LaunchSpeed = 100000; // 1000 m/s
 };
